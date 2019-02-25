d14ec63f06dbd6cee1931fbc67a08d786fffdda6
Zheeeng <hi@zheeeng.me>
2019-02-25 18:51:10 +0800

    Add const generate func

---
+++ const.go
@@ -0,0 +1,91 @@
+package pointer
+
+// Bool returns a pointer to bool type data
+func Bool(b bool) *bool {
+	return &b
+}
+
+// String returns a pointer to string type data
+func String(s string) *string {
+	return &s
+}
+
+// Int returns a pointer to int type data
+func Int(i int) *int {
+	return &i
+}
+
+// Int8 returns a pointer to int8 type data
+func Int8(i int8) *int8 {
+	return &i
+}
+
+// Int16 returns a pointer to int16 type data
+func Int16(i int16) *int16 {
+	return &i
+}
+
+// Int32 returns a pointer to int32 type data
+func Int32(i int32) *int32 {
+	return &i
+}
+
+// Int64 returns a pointer to int64 type data
+func Int64(i int64) *int64 {
+	return &i
+}
+
+// Uint returns a pointer to uint type data
+func Uint(u uint) *uint {
+	return &u
+}
+
+// Uint8 returns a pointer to uint8 type data
+func Uint8(u uint8) *uint8 {
+	return &u
+}
+
+// Uint16 returns a pointer to uint16 type data
+func Uint16(u uint16) *uint16 {
+	return &u
+}
+
+// Uint32 returns a pointer to uint32 type data
+func Uint32(u uint32) *uint32 {
+	return &u
+}
+
+// Uint64 returns a pointer to uint64 type data
+func Uint64(u uint64) *uint64 {
+	return &u
+}
+
+// Byte returns a pointer to byte type data
+func Byte(b byte) *byte {
+	return &b
+}
+
+// Rune returns a pointer to rune type data
+func Rune(r rune) *rune {
+	return &r
+}
+
+// Float32 returns a pointer to float32 type data
+func Float32(f float32) *float32 {
+	return &f
+}
+
+// Float64 returns a pointer to float64 type data
+func Float64(f float64) *float64 {
+	return &f
+}
+
+// Complex64 returns a pointer to complex64 type data
+func Complex64(c complex64) *complex64 {
+	return &c
+}
+
+// Complex128 returns a pointer to complex128 type data
+func Complex128(c complex128) *complex128 {
+	return &c
+}
